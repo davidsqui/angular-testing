e190cec4249fcf9add9f3c0f8a4e26384c3b61ab
David Sanchez <davidsqr6@gmail.com>
2022-09-23 15:43:04 -0500

    chore: configure mocka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Calculator } from './calculator';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'testing';
+
+  ngOnInit(): void {
+    const calculator = new Calculator();
+    const result = calculator.multiply(3,3);
+  }
+
 }
--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,38 +1,41 @@
 import { Calculator } from './calculator';
 
-fdescribe('Test for calculator', () => {
-  it('#multiply should return a nine', () => {
-    //Arrange
-    const calculator = new Calculator();
-    //Actions
-    const result = calculator.multiply(3, 3);
-    //Asserts
-    expect(result).toEqual(9);
+describe('Test for calculator', () => {
+  describe('Test for multiply', () => {
+    it('should return a nine', () => {
+      //Arrange
+      const calculator = new Calculator();
+      //Actions
+      const result = calculator.multiply(3, 3);
+      //Asserts
+      expect(result).toEqual(9);
+    });
+
+    it('should return a four', () => {
+      //Arrange
+      const calculator = new Calculator();
+      //Actions
+      const result = calculator.multiply(1, 4);
+      //Asserts
+      expect(result).toEqual(4);
+    });
   });
 
-  it('#multiply should return a four', () => {
-    //Arrange
-    const calculator = new Calculator();
-    //Actions
-    const result = calculator.multiply(1, 4);
-    //Asserts
-    expect(result).toEqual(4);
-  });
+  describe('Test for divide', () => {
+    it('should return a some numbers', () => {
+      //Arrange
+      const calculator = new Calculator();
+      //Asserts
+      expect(calculator.divide(6, 3)).toEqual(2);
+      expect(calculator.divide(5, 2)).toEqual(2.5);
+    });
 
-  it('#divide should return a some numbers', () => {
-    //Arrange
-    const calculator = new Calculator();
-    //Asserts
-    expect(calculator.divide(6, 3)).toEqual(2);
-    expect(calculator.divide(5, 2)).toEqual(2.5);
-  });
-
-  it('#divide for zero', () => {
-    //Arrange
-    const calculator = new Calculator();
-    //Actions
-    //Asserts
-    expect(calculator.divide(6, 0)).toBeNull();
-    expect(calculator.divide(5, 0)).toBeNull();
+    it('for zero', () => {
+      //Arrange
+      const calculator = new Calculator();
+      //Asserts
+      expect(calculator.divide(6, 0)).toBeNull();
+      expect(calculator.divide(5, 0)).toBeNull();
+    });
   });
 });
