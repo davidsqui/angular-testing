6ed6872d766578b9018d77c822d6613884f395eb
David Sanchez <davidsqr6@gmail.com>
2022-09-25 21:11:45 -0500

    feat: setup base project

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,24 @@
+import { Product } from './../../models/product.model';
+import { ProductsService } from './../../services/product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+})
+export class ProductsComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.productsService.getAllSimple().subscribe((data) => {
+      this.products = data;
+    });
+  }
+}
